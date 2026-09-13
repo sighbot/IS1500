75bee724e85415344feb2e4a148c9369e4650223
lovwic <lovwic@kth.se>
2026-09-13 12:10:26 +0200

    Assign. 3.1: Fix program logic.

--- labs/lab2/lab2-files/prime.c
+++ labs/lab2/lab2-files/prime.c
@@ -2,7 +2,7 @@
  prime.c
  By David Broman.
 
-Modified by Lovisa Wicklander
+ Modified by Lovisa Wicklander
 
  Last modified: 2026-09-13
  This file is in the public domain.
@@ -25,18 +25,17 @@ int is_prime(int n){
     return 0; // if divisible by 2, n is not a prime number ("takes out" the even numbers)
   }
 
-  for(int i = 0; i < 4; i++) {
-    int p = 3 + (2*i); // to get prime numbers 3, 5, 7 & 9
-    if(n % p == 0) { 
+  for(int i = 3; i <= n/2; i = i + 2) {
+    if(n % i == 0) { 
       return 0; // if divisible by any of the indivisible prime numbers, it is not a prime number
-    } else {
-      return 1; // if not, then it is a prime number
-    }
+    } 
   }
+
+  return 1; // if not, then it is a prime number
 
 }
 int main(void){
-  printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
+  printf("%d\n", is_prime(11)); // 11 is a prime.     Should print 1.
   printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
   printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
 }
