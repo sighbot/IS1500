4d60e2a2fa7d00913d4df7a2278eb079235c0c31
lovwic <lovwic@kth.se>
2026-09-13 11:43:04 +0200

    Assignment 3.1: create solution for is-prime function.

--- labs/lab2/lab2-files/prime.c
+++ labs/lab2/lab2-files/prime.c
@@ -1,7 +1,10 @@
 /*
  prime.c
  By David Broman.
- Last modified: 2015-09-15
+
+Modified by Lovisa Wicklander
+
+ Last modified: 2026-09-13
  This file is in the public domain.
 */
 
@@ -9,7 +12,28 @@
 #include <stdio.h>
 
 int is_prime(int n){
-    return 0;
+
+  if (n <= 1) {
+    return 0; // n is not a prime number
+  }  
+
+  if (n == 2) {
+    return 1; // n is a prime number
+  }
+
+  if (n % 2 == 0) {
+    return 0; // if divisible by 2, n is not a prime number ("takes out" the even numbers)
+  }
+
+  for(int i = 0; i < 4; i++) {
+    int p = 3 + (2*i); // to get prime numbers 3, 5, 7 & 9
+    if(n % p == 0) { 
+      return 0; // if divisible by any of the indivisible prime numbers, it is not a prime number
+    } else {
+      return 1; // if not, then it is a prime number
+    }
+  }
+
 }
 int main(void){
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
